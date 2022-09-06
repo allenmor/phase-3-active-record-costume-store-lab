8518af3b8128e0aa2cce2930fb404eafc8138cb0
Allen Mordukhaev <allenmor93@gmail.com>
2022-09-06 14:50:58 -0400

    commit tree f062552095085c34eb1e628395aa7b9e575b58c9

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,8 @@
 # Create your HauntedHouse class here
+
+class HauntedHouse < ActiveRecord::Base
+    def apply_discount(discount)
+        self.price = self.price * discount
+        self.save
+    end
+end
